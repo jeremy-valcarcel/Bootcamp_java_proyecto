ba0fda6bb3d32ffebea28d44a32de9533840625b
AlderetesMariaCandela <137343271+AlderetesMariaCandela@users.noreply.github.com>
2023-09-12 11:16:52 -0300

    actualizacion usurio

--- proyecto/src/main/java/com/bootcamp/proyecto/models/Usuario.java
+++ proyecto/src/main/java/com/bootcamp/proyecto/models/Usuario.java
@@ -1,5 +1,38 @@
 package com.bootcamp.proyecto.models;
 
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Usuario")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	@Not
+	@Email(message="ingrese...")
+	private String email;
+	
+	@NotBlank(message="Por favor, ingresa el password")
+	@Size(min=8, max=64, message= "Password debe contener minimo 8 caracteres")
+	private String password;
+	
+	@Transient
+	@NotBlank(message="Por favor confirma la contrasenia")
+	@Size(min=8, message= "Password debe contener minimo 8 caracteres")
+	private String passwordConfirmation;
+	
+	@Column(updatable = false)
+	private Date createdAt;
+	private Date updatedAt;
+	
+	
 
 }
